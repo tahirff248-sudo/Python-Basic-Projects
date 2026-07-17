52d277df91eec1c4faf85d5cd44f5db00ea716fa
Muhammad Tahir <tahirff248@gmail.com>
2026-07-17 05:14:44 +0500

    Updated password_generator

--- Password_generator.py
+++ Password_generator.py
@@ -3,20 +3,60 @@
 import random
 
 while True:
-    try:   #error handling
-        a = "== PASSWORD GENERATOR ==\n"
-        print(a.center(60))
-        name = input("Enter your name: ").lower()
-        password = str(input("Enter password length (At least 6 characters): ")).upper()
+    try:                                                 # error handling
+        title1 = "== PASSWORD GENERATOR ==\n"   
+        print(title1.center(60))
+
+        print("1 - Generate pass")      
+        print("2 - Exit\n")
+
+        choice1  = int(input("Enter choice(1-2): "))
+
+        if choice1 == 1:                                 # if-else conditions
+            num = random.randint(1,999)
     
-        symbol = input("Enter any symbols: ")
-        num = input("Enter any numbers: ")
+            name = input("\nEnter your name (5-digits): ").lower()      # 5-digits name conditon
     
-        all = password + name + symbol + num
-        reverse = all[::-1]
+            if len(name) <= 5:
+                pass
+            else:
+                print("Name must be of 5 digits or less!")
+                break
     
-        print(f"Generated password: {reverse}")
+            short_pass = input("Enter password length (At least 3 characters): ").upper()    # 3-digits pass condition
     
-    except Exception as e:
+            if len(short_pass) <= 4:
+                pass
+    
+            else:
+                print("Password must be of 4 digits or less!")
+                break
+        
+            symbol = input("Enter any 3 symbols (@/#/%): ")               # 3-digits symbol condition
+    
+            if len(symbol) <= 3:
+                pass
+    
+            else:
+                print("symbol must be of 3 digits or less!")
+                break
+        
+            all =  short_pass + symbol + name            
+            reverse = all[::-1]                          # reverse string
+    
+            work = str(num)                # convert int to string
+    
+            done = reverse + work          # concatenate
+            
+            print(f"Generated password: {done}")       # password generation complete
+
+        elif choice1 == 2:
+            print("Thanks for using this program:)")        # Exiting program
+
+        else:
+            print("Invalid option!")      # non-valid condition
+            break
+        
+    except Exception as e:              
         print(f"Error occurred: {e}")
         break
